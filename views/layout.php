27c59cf78fbab4cc095f8b7abc6f49c25f480b31
Ehab <albostami@gmail.com>
2017-03-24 00:03:26 +0400

    Added clean routes for public files, ccreated a master.css file, linked to master.css in layout.php

--- views/layout.php
+++ views/layout.php
@@ -1,9 +1,11 @@
 <html>
+ <head>
+	 <!--  -->
+	 <link href="<?php echo $this->make_route('/css/master.css')?>" rel="stylesheet" type="text/css" />
+ </head>	 
  <body>
   <h1>Verge</h1>
-  <?php 
-    //echo $this->content . '<br/>';
-   include($this->content);
-   ?>  
+  <!-- <?php echo $this->content ;?> -->
+  <?php include($this->content);?>  
  </body>
 </html>
